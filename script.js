88ab1edacd0f4455e4b4ed88f8fb77e8fbd32b42
wajid ansari <112863100+ansariwajid9999@users.noreply.github.com>
2024-10-15 16:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your code here
+const image = document.querySelector(".image")
+const age = document.getElementById("age")
+const email = document.getElementById("email")
+const phone = document.getElementById("phone")
+const getAnother = document.getElementById("getAnother")
+const ParaName = document.getElementById("ParaName")
+const displayInfo = document.getElementById("displayInfo")
+
+async function getApi(){
+    const apiUrl = "https://randomuser.me/api/"
+    const response = await fetch(apiUrl)
+    if(!response.ok){
+        throw new Error("Could not fetch api data");
+    }
+    return await response.json()
+}
+
+async function Helper() {
+    const response = await getApi()
+    image.src = response.results[0].picture.large
+    
+    ParaName.textContent = response.results[0].name.first + " " + response.results[0].name.last
+
+    age.onclick = () => {
+        displayInfo.textContent = response.results[0].dob.age
+    }
+    email.onclick = () => {
+        displayInfo.textContent = response.results[0].email
+    }
+    phone.onclick = () => {
+        displayInfo.textContent = response.results[0].phone
+    }
+}
+getAnother.onclick = () => {
+    displayInfo.textContent = ""
+    Helper()
+}
+Helper()
